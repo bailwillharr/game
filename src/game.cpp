ea8fa93ad79aea45822fdb550fb70d6f35fa13c6
bailwillharr <baileyharrison04@outlook.com>
2022-09-04 01:32:24 +0100

    Update engine; remove getComponent<Transform>'s

--- src/game.cpp
+++ src/game.cpp
@@ -1,4 +1,4 @@
-#include "game.hpp"
+#include <config.h>
 
 #include <log.hpp>
 
@@ -13,7 +13,6 @@
 
 #include "components/text_ui_renderer.hpp"
 #include "components/mesh_renderer.hpp"
-#include "components/transform.hpp"
 #include "components/camera.hpp"
 
 using namespace components;
@@ -27,12 +26,11 @@ public:
 
 	void onUpdate(glm::mat4) override
 	{
-		auto t = parent.getComponent<Transform>();
 		float dt = parent.win.dt();
 		yaw += dt * speed;
 		yaw = glm::mod(yaw, glm::two_pi<float>());
-		t->rotation = glm::angleAxis(yaw, glm::vec3{0.0f, 1.0f, 0.0f});
-		t->rotation *= glm::angleAxis(pitch, glm::vec3{1.0f, 0.0f, 0.0f});
+		parent.transform.rotation = glm::angleAxis(yaw, glm::vec3{0.0f, 1.0f, 0.0f});
+		parent.transform.rotation *= glm::angleAxis(pitch, glm::vec3{1.0f, 0.0f, 0.0f});
 	}
 private:
 	float yaw = 0.0f;
@@ -46,7 +44,7 @@ static void createMenu(SceneRoot& menu);
 void playGame()
 {
 
-	Window win("sdltest");
+	Window win(PROJECT_NAME);
 	Input input(win); /* Input Manager */
 	ResourceManager resMan;
 
@@ -70,16 +68,16 @@ static void createMenu(SceneRoot& menu)
 	menu.createChild("cam")->createComponent<Camera>()->useOrtho();
 	menu.createChild("text")->createComponent<UI>()->m_text = "PAUSED";
 	menu.getChild("text")->getComponent<UI>()->m_color = {1.0f, 0.0f, 0.0f};
-	menu.getChild("text")->getComponent<Transform>()->position = {-1.0f, 0.90f, 0.0f};
+	menu.getChild("text")->transform.position = {-1.0f, 0.90f, 0.0f};
 	menu.createChild("text2")->createComponent<UI>()->m_text = "Press enter to play";
-	menu.getChild("text2")->getComponent<Transform>()->position = {-1.0f, 0.70f, 0.0f};
+	menu.getChild("text2")->transform.position = {-1.0f, 0.70f, 0.0f};
 	menu.createChild("text3")->createComponent<UI>()->m_text = "Press escape to quit";
-	menu.getChild("text3")->getComponent<Transform>()->position = {-1.0f, 0.50f, 0.0f};
+	menu.getChild("text3")->transform.position = {-1.0f, 0.50f, 0.0f};
 
 	menu.createChild("donut")->createComponent<Renderer>()->setMesh("meshes/donut.mesh");
-	menu.getChild("donut")->getComponent<Transform>()->position = {0.0f, 0.0f, -5.0f};
-	menu.getChild("donut")->getComponent<Transform>()->scale = {0.025f, 0.025f, 0.025f};
-	menu.getChild("donut")->getComponent<Transform>()->rotation = glm::angleAxis(glm::half_pi<float>(), glm::vec3{1.0f, 0.0f, 0.0f});
+	menu.getChild("donut")->transform.position = {0.0f, 0.0f, -5.0f};
+	menu.getChild("donut")->transform.scale = {0.025f, 0.025f, 0.025f};
+	menu.getChild("donut")->transform.rotation = glm::angleAxis(glm::half_pi<float>(), glm::vec3{1.0f, 0.0f, 0.0f});
 
 	menu.getChild("donut")->createComponent<Spin>()->pitch = glm::half_pi<float>();
 	menu.getChild("donut")->getComponent<Spin>()->speed = 4.0f;
@@ -114,22 +112,21 @@ static void addObjects(SceneRoot& mainScene)
 	constexpr float HEIGHT_INCHES = 6.0f * 12.0f;
 	// eye level is about 4 1/2 inches below height
 	constexpr float EYE_LEVEL = (HEIGHT_INCHES - 4.5f) * 25.4f / 1000.0f;
-	cam->getComponent<Transform>()->position = { 0.0f, EYE_LEVEL, 0.0f };
+	cam->transform.position = { 0.0f, EYE_LEVEL, 0.0f };
 	auto camCamera = cam->createComponent<Camera>();
 	camCamera->usePerspective(70.0f);
 	cam->createComponent<CameraController>();
-	camCamera->m_noClear = false;
+	camCamera->m_noClear = true;
 	camCamera->clearColor = { 0.5f, 0.5f, 0.5f, 1.f };
 	cam->createComponent<Renderer>()->m_mesh = genSphereMesh(0.2f, 20);
 	cam->getComponent<Renderer>()->setTexture("textures/cobble_stone.png");
 
 	auto gun = cam->createChild("gun");
-	auto gunTransform = gun->getComponent<Transform>();
-	gunTransform->position = glm::vec3{0.2f, -0.1f, -0.15f};
-	gunTransform->rotation = glm::angleAxis(glm::pi<float>(), glm::vec3{ 0.0f, 1.0f, 0.0f });
+	gun->transform.position = glm::vec3{0.2f, -0.1f, -0.15f};
+	gun->transform.rotation = glm::angleAxis(glm::pi<float>(), glm::vec3{ 0.0f, 1.0f, 0.0f });
 	float GUN_SCALE = 9.0f / 560.0f;
 	GUN_SCALE *= 1.0f;
-	gunTransform->scale *= GUN_SCALE;
+	gun->transform.scale *= GUN_SCALE;
 	auto gunRenderer = gun->createComponent<Renderer>();
 	gunRenderer->setMesh("meshes/gun.mesh");
 	gunRenderer->setTexture("textures/gun.png");
@@ -141,22 +138,32 @@ static void addObjects(SceneRoot& mainScene)
 	class FPSCount : public CustomComponent {
 	public:
 		UI* ui;
-		Transform* t;
+
+		uint64_t lastAction;
 
 		FPSCount(Object* parent) : CustomComponent(parent)
 		{
 			ui = parent->getComponent<UI>();
-			t = parent->getComponent<Transform>();
-			t->position.x = 1.0f;
-			t->position.y = 0.95f;
-			t->scale = {0.5f, 0.5f, 1.0f};
+			parent->transform.position.x = 1.0f;
+			parent->transform.position.y = 0.95f;
+			parent->transform.scale = {0.5f, 0.5f, 1.0f};
 			ui->m_alignment = components::UI::Alignment::RIGHT;
+
+			lastAction = win.getNanos();
 		}
 
 		void onUpdate(glm::mat4) override
 		{
-			ui->m_text = "FPS: " + std::to_string(win.getFPS());
-			ui->m_text += " " + std::to_string(win.dt() * 1000.0f) + " ms";
+			
+
+			constexpr float DELAY = 0.5f;
+			const uint64_t now = win.getNanos();
+			if (now - lastAction > DELAY * BILLION) {
+				lastAction = now;
+				win.setTitle(std::to_string(win.getFPS()));
+				ui->m_text = "FPS: " + std::to_string(win.getFPS());
+				ui->m_text += " " + std::to_string(win.dt() * 1000.0f) + " ms";
+			}
 		}
 	};
 
@@ -170,9 +177,8 @@ static void addObjects(SceneRoot& mainScene)
 	
 	constexpr float GRASS_DENSITY = 128.0f;
 	auto floor = mainScene.createChild("floor");
-	auto floorTransform = floor->getComponent<Transform>();
 	auto floorRenderer = floor->createComponent<Renderer>();
-	floorTransform->position = glm::vec3{ 0.0f, 0.0f, 0.0f };
+	floor->transform.position = glm::vec3{ 0.0f, 0.0f, 0.0f };
 	floorRenderer->setTexture("textures/stone_bricks.png");
 	floorRenderer->m_mesh = std::make_unique<resources::Mesh>(std::vector<Vertex>{
 		{ { -16.0f, 0.0f, 16.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f,  GRASS_DENSITY } },
@@ -184,7 +190,7 @@ static void addObjects(SceneRoot& mainScene)
 		{ { -16.0f, 0.0f,  16.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f,  GRASS_DENSITY } },	
 		
 	});
-	floorTransform->scale = { 4.0f, 4.0f, 4.0f };
+	floor->transform.scale = { 4.0f, 4.0f, 4.0f };
 
 
 
@@ -198,7 +204,7 @@ static void addObjects(SceneRoot& mainScene)
 
 	auto chunk = mainScene.createChild("chunk");
 	chunk->createComponent<Renderer>()->m_mesh = getChunkMesh(0, 0);
-	chunk->getComponent<Transform>()->position = {8.0f, 1.0f, 8.0f};
+	chunk->transform.position = {8.0f, 1.0f, 8.0f};
 
 
 
@@ -207,25 +213,24 @@ static void addObjects(SceneRoot& mainScene)
 
 	auto monke = mainScene.createChild("monke");
 	auto monkeRen = monke->createComponent<Renderer>();
-	auto monkeTfm = monke->getComponent<Transform>();
 	
 	monkeRen->setMesh("meshes/monke.mesh");
 	monkeRen->setTexture("textures/MonkeTexture.png");
 	monkeRen->m_emission = { 0.05f, 0.05f, 0.05f };
 
-	monkeTfm->position = { -10.0f, 1.0f, 10.0f };
+	monke->transform.position = { -10.0f, 1.0f, 10.0f };
 
 
 
 	auto earth = mainScene.createChild("earth");
-	earth->getComponent<Transform>()->position = { 0.0f, 7.0f, -15.0f };
+	earth->transform.position = { 0.0f, 7.0f, -15.0f };
 	auto earthRenderer = earth->createComponent<Renderer>();
 	earthRenderer->setTexture("textures/grass_block_top.png");
 	earthRenderer->m_color = { 145.0f/255.0f, 189.0f/255.0f, 89/255.0f };
 	earth->createComponent<Spin>();
 	mainScene.createChild("detail_text")->createComponent<UI>();
-	mainScene.getChild("detail_text")->getComponent<Transform>()->position = { 0.0f, -0.95f, 0.0f };
-	mainScene.getChild("detail_text")->getComponent<Transform>()->scale = { 0.5f, 0.5f, 1.0f };
+	mainScene.getChild("detail_text")->transform.position = { 0.0f, -0.95f, 0.0f };
+	mainScene.getChild("detail_text")->transform.scale = { 0.5f, 0.5f, 1.0f };
 
 	class DetailAdjust : public CustomComponent {
 	public:
@@ -282,7 +287,7 @@ static void addObjects(SceneRoot& mainScene)
 
 		void setText()
 		{
-			text->m_text = "FORTNITE: " + std::to_string(detail) + " STEP: " + std::to_string(step);
+			text->m_text = "DETAIL: " + std::to_string(detail) + " STEP: " + std::to_string(step);
 		}
 	};
 
@@ -292,13 +297,13 @@ static void addObjects(SceneRoot& mainScene)
 	auto cuboidRen = mainScene.createChild("cuboid")->createComponent<Renderer>();
 	cuboidRen->setMesh("meshes/cube.mesh");
 	cuboidRen->setTexture("textures/cobble_stone.png");
-	mainScene.getChild("cuboid")->getComponent<Transform>()->scale = {0.1f, 5.0f, 5.0f};
-	mainScene.getChild("cuboid")->getComponent<Transform>()->position = {-65.0f, 5.0f, 0.0f};
+	mainScene.getChild("cuboid")->transform.scale = {0.1f, 5.0f, 5.0f};
+	mainScene.getChild("cuboid")->transform.position = {-65.0f, 5.0f, 0.0f};
 
 	auto sunPivot = mainScene.createChild("sun_pivot");
-	sunPivot->getComponent<Transform>()->position = { 0.0f, 10.0f, 0.0f };
+	sunPivot->transform.position = { 0.0f, 10.0f, 0.0f };
 	auto sun = sunPivot->createChild("sun");
-	sun->getComponent<Transform>()->position = { 50.0f, 0.0f, 0.0f };
+	sun->transform.position = { 50.0f, 0.0f, 0.0f };
 	auto sunRen = sun->createComponent<Renderer>();
 	sunRen->m_mesh = genSphereMesh(2.0f, 16);
 	sunRen->m_color = { 1.0f, 1.0f, 1.0f };
@@ -308,7 +313,6 @@ static void addObjects(SceneRoot& mainScene)
 	public:
 		SunMovement(Object* parent) : CustomComponent(parent)
 		{
-			sunPivotTransform = parent->getParent()->getComponent<Transform>();
 		}
 
 		void onUpdate(glm::mat4 t) override
@@ -320,10 +324,9 @@ static void addObjects(SceneRoot& mainScene)
 			
 			yaw += dt * speed;
 			yaw = glm::mod(yaw, glm::two_pi<float>());
-			sunPivotTransform->rotation = glm::angleAxis(yaw, glm::vec3{ 0.0f, 1.0f, 0.0f });
+			parent.getParent()->transform.rotation = glm::angleAxis(yaw, glm::vec3{0.0f, 1.0f, 0.0f});
 		}
 	private:
-		Transform* sunPivotTransform;
 		float yaw = 0.0f;
 		float speed = 1.0f;
 	};
